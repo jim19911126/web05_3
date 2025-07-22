e0fa2805f18c336f38e30f92bae88450b9504a02
jimlee <jim19911126@gmail.com>
2025-07-22 11:26:08 +0800

    預告片清單頁面與資料庫建立

--- back/poster.php
+++ back/poster.php
@@ -1,1 +1,21 @@
-海報
+<div style="height: 320px;">
+    <h3 class="ct">預告片清單</h3>
+
+</div>
+<hr>
+<div style="height: 160px;">
+    <h3 class="ct">新增預告片海報</h3>
+    <form action="./api/add_poster.php" method="post">
+        <table>
+            <td>預告片海報:</td>
+            <td><input type="file" name="file" id="file"></td>
+            <td>預告片片名:</td>
+            <td><input type="text" name="text" id="text"></td>
+        </table>
+        <div class="ct">
+            <input type="submit" value="新增">
+            <input type="reset" value="重置">
+        </div>
+    </form>
+
+</div>
