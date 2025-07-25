150fb9ba01c80c779513e2da61ceee19c7e9fe73
jimlee <jim19911126@gmail.com>
2025-07-25 14:09:55 +0800

    新增電影版面完成

--- back/movie.php
+++ back/movie.php
@@ -1,1 +1,36 @@
-原線片
+<button onclick="location.href='?do=add_movie'">新增電影</button>
+<hr>
+
+<style>
+    .movie{
+        display: flex  ;
+        width: 95%;
+        margin: auto;
+        box-shadow: 0 0 3px #999;
+    }
+</style>
+
+<div class="movie">
+    <div>
+        <img src="" alt="">
+    </div>
+    <div>
+        分級
+
+    </div>
+    <div>
+        <div>
+
+        </div>
+        <div>
+            <button>顯示</button>
+            <button>往上</button>
+            <button>往下</button>
+            <button>編輯電影</button>
+            <button>刪除電影</button>
+        </div>
+        <div>
+            劇情介紹
+        </div>
+    </div>
+</div>
