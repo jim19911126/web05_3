2ff350ae1cc78afb023d3c12eba6289bc8e6fcaa
jimlee <jim19911126@gmail.com>
2025-08-01 09:43:26 +0800

    劃位的座位圖完成

--- api/get_booking.php
+++ api/get_booking.php
@@ -1,4 +1,32 @@
-劃位
+<style>
+    .booking-box{
+        width:540px;
+        height:370px;
+        background:url("./icon/03D04.png") no-repeat center;
+        margin:0 auto;
+    }
+    .info-box{
+        background:#ddd;
+        width:540px;
+        margin:10px auto;
+        padding:10px 70px;
+        box-sizing:border-box;
+    }
+</style>
 
-<button class="btn-prev">上一步</button>
-<button class="btn-book">訂購</button>
+<div class="booking-box">
+
+</div>
+
+<div class="info-box">
+    <div class="order-info">
+        <div>您選擇的電影是：</div>
+        <div>您選擇的時刻是：</div>
+        <div>您已經勾選<span id="tix">0</span>張票，最多可以購買四張票</div>
+    </div>
+</div>
+
+<div class="ct">
+    <button class="btn-prev">上一步</button>
+    <button class="btn-book">訂購</button>
+</div>
--- front/order.php
+++ front/order.php
@@ -1,29 +1,32 @@
 <style>
-#orderTable{
-    width:50%;
-    margin:10px auto;
-    padding:20px;
-    border:1px solid #ccc;
-    background:#aaa;
-}    
-#orderTable td{
-    background:#ccc;
-}
-#orderTable td:nth-child(1){
-    width:20%;
-    text-align:right;
-}
-#orderTable td:nth-child(2) select{
-    width:98%;
-    text-align:left;
-}
+    #orderTable {
+        width: 50%;
+        margin: 10px auto;
+        padding: 20px;
+        border: 1px solid #ccc;
+        background: #aaa;
+    }
+
+    #orderTable td {
+        background: #ccc;
+    }
+
+    #orderTable td:nth-child(1) {
+        width: 20%;
+        text-align: right;
+    }
+
+    #orderTable td:nth-child(2) select {
+        width: 98%;
+        text-align: left;
+    }
 </style>
 
 <div id="orderForm">
-    
+
     <h3 class="ct">線上訂票</h3>
-    
-    
+
+
     <table id="orderTable">
         <tr>
             <td>電影</td>
@@ -45,63 +48,70 @@
 </div>
 
 <div id="assign" style="display: none;">
-劃位
 
-<button class="btn-prev">上一步</button>
-<button class="btn-book">訂購</button>
 </div>
 
 <script>
-    let url=new URLSearchParams(location.search);
+    let url = new URLSearchParams(location.search);
 
     getMovie();
 
-    $("#movie").on("change",function name(params) {
+    $("#movie").on("change", function name(params) {
         getDate($(this).val())
     })
 
-    $("#date").on("change",function name(params) {
-        getSession($("#movie").val(),$(this).val())
-        
+    $("#date").on("change", function name(params) {
+        getSession($("#movie").val(), $(this).val())
+
     })
 
     // 按鈕切換
-    $(".btn-submit").on("click",function name(params) {
+    $(".btn-submit").on("click", function name(params) {
+
+        $.get("./api/get_booking.php", {
+            id: $("#movie").val(),
+            date: $('#date').val(),
+            session: $('#session').val()
+        }, (booking) => {
+            $('#assign').html(booking);
+            
+            $(".btn-prev").on("click", function name(params) {
+                $("#assign").hide();
+                $("#orderForm").show();
+            })
+        })
         $("#orderForm").hide();
         $("#assign").show();
 
     })
 
-    $(".btn-prev").on("click",function name(params) {
-        $("#assign").hide();
-        $("#orderForm").show();
-    })
+
 
     function getMovie(params) {
-        let id=0
-        if(url.has('id')){
-            id=url.get('id');
+        let id = 0
+        if (url.has('id')) {
+            id = url.get('id');
         }
-        $.get("./api/get_movie.php",{id},(movies)=>{
+        $.get("./api/get_movie.php", { id }, (movies) => {
             $('#movie').html(movies)
-            
+
             getDate($("#movie").val());
         })
     }
 
-    function  getDate(movieId) {
-        $.get("./api/get_date.php",{movieId},(dates)=>{
+    function getDate(movieId) {
+        $.get("./api/get_date.php", { movieId }, (dates) => {
             $("#date").html(dates)
 
-            getSession(movieId,$('#date').val())
+            getSession(movieId, $('#date').val())
         })
-        
+
     }
 
-    function getSession(movieId,date) {
-        $.get("./api/get_session.php",{movieId,date},(sessions)=>{
+    function getSession(movieId, date) {
+        $.get("./api/get_session.php", { movieId, date }, (sessions) => {
             $("#session").html(sessions)
         })
-        
+
     }
 </script>
